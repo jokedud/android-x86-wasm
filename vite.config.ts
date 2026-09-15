7458a5d89ca03e707e1d84c92646391d26e1cbab
jokedud <156004109+jokedud@users.noreply.github.com>
2026-09-15 00:58:04 -0400

    Fix Vercel asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/android-x86-wasm/",
+  base: "/",
   build: {
     target: "es2022",
     sourcemap: true,
